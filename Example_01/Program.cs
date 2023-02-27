e1a0d3a53bc3fc5d434c947fac9279ac9fd1a560
FirLanPro <FirLanPro@yandex.ru>
2023-02-27 10:36:54 +0300

    код

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n%7==0 && n%23==0)
+{
+    Console.Write("делится");
+   
+}
+else 
+{
+   Console.Write("не делится ");
+ 
+}
